ed90b1248d232e7621b69d946f8378df19344456
mahsa <87281261+mahsakhorshiddoust@users.noreply.github.com>
2022-12-03 16:11:47 +0300

    adds-shows-list

--- src/components/MovieListing/MovieListing.js
+++ src/components/MovieListing/MovieListing.js
@@ -1,13 +1,15 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import { getAllMovies } from "../../features/movies/movieSlice";
+import { getAllMovies, getAllShows } from "../../features/movies/movieSlice";
 import MovieCard from "../MovieCard/MovieCard";
 import "./MovieListing.scss";
 
 function MovieListing() {
   const movies = useSelector(getAllMovies);
+  const shows = useSelector(getAllShows);
 
-  let renderMovies = "";
+  let renderMovies,
+    renderShows = "";
 
   renderMovies =
     movies.Response === "True" ? (
@@ -20,11 +22,24 @@ function MovieListing() {
       </div>
     );
 
+  renderShows =
+    shows.Response === "True" ? (
+      shows.Search.map((show, index) => <MovieCard key={index} data={show} />)
+    ) : (
+      <div className="shows-error">
+        <h3>shows.Error</h3>
+      </div>
+    );
+
   return (
     <div className="movie-wrapper">
       <div className="movie-list">
         <h2>Movies</h2>
         <div className="movie-container">{renderMovies}</div>
+      </div>
+      <div className="shows-list">
+        <h2>Shows</h2>
+        <div className="movie-container">{renderShows}</div>
       </div>
     </div>
   );
